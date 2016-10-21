d71cbc6ad2f9dc83feadf8b868ed31bc72eb512f
Oreoluwa Banwo <oreban4u@yahoo.co.uk>
2016-10-21 15:13:07 +0100

    Created Data Set and User Login function

---
+++ functions.js
@@ -0,0 +1,15 @@
+userData = {'oreban4u':{'password:'12345','firstname':'Oreoluwa','lastname':'Banwo'},
+             'francis':{'password:'12345','firstname':'Oreoluwa','lastname':'Banwo'},
+             'ralph':{'password:'12345','firstname':'Oreoluwa','lastname':'Banwo'},
+             'abdul':{'password:'12345','firstname':'Oreoluwa','lastname':'Banwo'}
+            }
+    
+
+function createUser(username, password){
+    if (password === userData.username.password){
+        return userData.username;
+    }
+    else{
+        return "Invalid Username or Password";
+    }
+}
